90fabee630dbfa6eef886b7ba558395c11b1546a
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-09 15:58:21 +0900

    update

--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,15 +1,26 @@
-import * as React from "react";
-import { Text, Box } from "native-base";
+import * as React from 'react';
+import { Text, Box, Pressable } from 'native-base';
 
 // Add Props in Hello({navigation})
-export default function Hello() {
+export default function Hello({ navigation }) {
   return (
     <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         Life's too short
       </Text>
 
-      {/* Create Button */}
+      <Pressable onPress={() => navigation.navigate('IncDec')}>
+        <Text
+          style={{
+            backgroundColor: 'green',
+            padding: 10,
+            marginTop: 20,
+            borderRadius: 10,
+          }}
+        >
+          Screen Increment and Decrement
+        </Text>
+      </Pressable>
     </Box>
   );
 }
