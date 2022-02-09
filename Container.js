744b4a128ccadef14ccb35da7112f75dbd171050
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-09 16:22:30 +0900

    update

--- Container.js
+++ Container.js
@@ -1,29 +1,86 @@
-import * as React from "react";
+import * as React from 'react';
 
 //Import Navigation Container
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native';
 
 // Import Stack Navigation
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 
 //Import Bottom Tab Navigation
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //Import Icon
+import { Ionicons } from '@expo/vector-icons';
 
 // Import Theme Native Base
-import { useTheme } from "native-base";
+import { useTheme } from 'native-base';
 
 // Import Screen
-import FormNativeBase from "./src/screens/formNativeBase";
-import Hello from "./src/screens/hello";
-import IncDec from "./src/screens/incDec";
+import FormNativeBase from './src/screens/formNativeBase';
+import Hello from './src/screens/hello';
+import IncDec from './src/screens/incDec';
 
 // Create Stack Navigation
 const Stack = createStackNavigator();
 
 //Create Bottom Tab Navigation
+const Tab = createBottomTabNavigator();
 
 // Create Component Bottom Tab Navigation
+function MyTab() {
+  const theme = useTheme();
+  return (
+    <Tab.Navigator
+      initialRouteName="Hello"
+      screenOptions={({ route }) => ({
+        headerMode: 'screen',
+        headerTintColor: 'white',
+        headerStyle: { backgroundColor: theme.colors.primary['300'] },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name == 'Hello') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name == 'Form') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name == 'Form1') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: theme.colors.primary['800'],
+        tabBarInactiveTintColor: 'grey',
+      })}
+    >
+      <Tab.Screen
+        name="Hello"
+        component={Hello}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Form"
+        component={FormNativeBase}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Form1"
+        component={FormNativeBase}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function Container() {
   // Init Theme
@@ -31,26 +88,19 @@ export default function Container() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerMode: "screen",
-          headerTintColor: "white",
-          headerStyle: { backgroundColor: theme.colors.primary["300"] },
-        }}
-      >
+      <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          component={Hello}
+          name="Main"
+          component={MyTab}
           options={{
-            title: "Hello Screen",
+            headerShown: false,
           }}
         />
         <Stack.Screen
           name="IncDec"
           component={IncDec}
           options={{
-            title: "Increment Decrement",
+            title: 'Increment Decrement',
           }}
         />
       </Stack.Navigator>
