2a39ddb9672942e42df86c958ca1faf62ecc33a5
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-09 19:25:56 +0900

    update

--- src/screens/DetailPost.js
+++ src/screens/DetailPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -6,29 +6,70 @@ import {
   StyleSheet,
   FlatList,
   RefreshControl,
-} from "react-native";
-import { ListItem, Avatar } from "react-native-elements";
+} from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 
 // Import Axios
+import axios from 'axios';
 
 const PostDetail = (props) => {
   //init Props
+  const { id, title, body } = props.route.params;
 
   //Init State
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Create LifeCycle
-  //Function Exception
+  useEffect(() => {
+    //Function Exception
+    getComments();
+  }, []);
 
   // Create Function to fetch
+  const getComments = () => {
+    setIsLoading(true);
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      .then((res) => {
+        setComments(res.data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        alert('Error Fetch Data');
+        setIsLoading(false);
+      });
+  };
 
   //   Create Component List
+  const _renderItem = ({ item }) => {
+    return (
+      <ListItem key={item.id.toString()} bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title numberOfLines={1}>{item.email}</ListItem.Title>
+          <ListItem.Subtitle numberOfLines={2}>
+            {item.name} - {item.body}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
+
   return (
     <View style={style.container}>
-      <Text h2 style={{ fontWeight: "bold" }}>
-        This Is Post Detail
+      <Text>This Is Post Detail</Text>
+      <Text h2 style={{ marginTop: 20, fontWeight: 'bold' }}>
+        {title}
       </Text>
-      {/* Implement Axios Here */}
-      {/* Render Component List */}
+      <Text>{body}</Text>
+      <Text style={{ marginTop: 20 }}>Comments</Text>
+      <FlatList
+        data={comments}
+        renderItem={_renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        refreshing={isLoading}
+        onRefresh={getComments}
+      />
     </View>
   );
 };
@@ -37,7 +78,7 @@ export default PostDetail;
 
 const style = StyleSheet.create({
   container: {
-    backgroundColor: "#FFF",
+    backgroundColor: '#FFF',
     padding: 16,
     flex: 1,
   },
